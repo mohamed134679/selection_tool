6f873637c1785dddcfecb3e81a2f64049d0bb640
hagaradel12 <hagaradel198@gmail.com>
2026-08-04 15:49:41 +0300

    Io routes

---
+++ backend/routes/io.js
@@ -0,0 +1,48 @@
+const express = required('express');
+const router = express.Router();
+
+
+const Io = require('../schemas/IO_schema');
+
+//create a new IO entry
+router.post('/io', async (req, res) => {
+    try {
+        const newIo = new Io(req.body);
+        await newIo.save();
+        res.status(201).json(newIo);
+    } catch(err){
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const ioList = await Io.find().populate('license');
+        res.json(ioList);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updated = await Io.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!updated) {
+            return res.status(404).json({ message: 'IO not found' });
+        }
+
+        res.json(updated);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
